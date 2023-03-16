9feedec53b517cbae6d9bfeabb495b9f62a940be
Andrew Hargrave <me@andyh.app>
2023-03-16 16:46:49 +0000

    fix stripe subscription created webhook

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -18,6 +18,60 @@ class WebhookController extends BaseWebhookController
         return DoctrineCashier::findBillable($stripeId);
     }
 
+    /**
+     * Handle customer subscription created.
+     *
+     * @param  array  $payload
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function handleCustomerSubscriptionCreated(array $payload)
+    {
+        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+
+        if ($user) {
+            $data = $payload['data']['object'];
+
+            $criteria = Criteria::create()
+                ->where(
+                    Criteria::expr()->eq('stripeId', $data['id']),
+                );
+
+            $subscription = $user->getSubscriptions()->matching($criteria)?->first();
+
+            if (! $subscription) {
+                if (isset($data['trial_end'])) {
+                    $trialEndsAt = Carbon::createFromTimestamp($data['trial_end']);
+                } else {
+                    $trialEndsAt = null;
+                }
+
+                $firstItem = $data['items']['data'][0];
+                $isSinglePrice = count($data['items']['data']) === 1;
+
+                $subscription = $user->subscriptions()->create([
+                    'name' => $data['metadata']['name'] ?? $this->newSubscriptionName($payload),
+                    'stripe_id' => $data['id'],
+                    'stripe_status' => $data['status'],
+                    'stripe_price' => $isSinglePrice ? $firstItem['price']['id'] : null,
+                    'quantity' => $isSinglePrice && isset($firstItem['quantity']) ? $firstItem['quantity'] : null,
+                    'trial_ends_at' => $trialEndsAt,
+                    'ends_at' => null,
+                ]);
+
+                foreach ($data['items']['data'] as $item) {
+                    $subscription->items()->create([
+                        'stripe_id' => $item['id'],
+                        'stripe_product' => $item['price']['product'],
+                        'stripe_price' => $item['price']['id'],
+                        'quantity' => $item['quantity'] ?? null,
+                    ]);
+                }
+            }
+        }
+
+        return $this->successMethod();
+    }
+
 //    TODO: Make this work via doctrine-eloquent
     /**
      * Handle a canceled customer from a Stripe subscription.
